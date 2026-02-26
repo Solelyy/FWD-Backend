74cf98f3f06b6e568566021214881258bad5525f
dinavelB <binongo.dinavel.escodero@gmail.com>
2026-02-26 12:28:51 +0800

    feat: user can now set a password in email verification
    clicked verify then sets a password then user becomes active and has a hashed password for security

---
+++ src/modules/auth/dto/setup-pass.dto.ts
@@ -0,0 +1,11 @@
+import { IsString, MinLength, Matches } from 'class-validator';
+
+export class SetPasswordDto {
+  @IsString()
+  @MinLength(8)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number or special character',
+  })
+  password: string;
+}
