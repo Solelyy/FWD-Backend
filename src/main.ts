4efdf7cd74679c32301e58fdcc6c98ee4f106e34
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2026-03-10 19:26:21 +0800

    changes for staging

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
+/*
 async function startServer() {
   const environment = process.env.NODE_ENV || 'development';
   const envPath = `.env.${environment}`; //change the env value
@@ -26,4 +27,35 @@ async function startServer() {
   console.log('ur at:', NODE_ENV);
   console.log('server starting at localhost:', PORT);
 }
+startServer();
+*/
+
+async function startServer() {
+  const environment = process.env.NODE_ENV || 'development';
+  const envPath = `.env.${environment}`;
+
+  env.config({ path: envPath });
+
+  const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+  if (!process.env.COOKIE_SECRET) {
+    throw new Error('COOKIE_SECRET is not defined. Set it in your environment variables (railway).');
+  }
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://fwd-frontend.vercel.app/',
+    ],
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`Environment: ${environment}`);
+  console.log(`Server running on port ${port}`);
+}
+
 startServer();
