1863e67eafd182cd09f05b7ac61810777e5e3dce
dinavelB <binongo.dinavel.escodero@gmail.com>
2026-03-20 10:57:20 +0800

    added status to active after suspension span

--- src/utils/cron.utils.ts
+++ src/utils/cron.utils.ts
@@ -1,23 +1,28 @@
 import { Cron } from '@nestjs/schedule';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma_global/prisma.service';
+import { Status } from '@prisma/client';
 @Injectable()
 export class CronCheckerHelper {
   constructor(private readonly prisma: PrismaService) {}
-  @Cron('0 * * * *')
+  //  test, remove one asterisk to check every hour
+  // this checks every minute
+  @Cron('0 * * * * *')
   async dateChecker() {
     const date = new Date();
 
     const findMany = await this.prisma.user.updateMany({
       where: {
-        endDate: { lt: date },
+        endDate: { lt: date }, // lt means, less than
+        status: Status.SUSPENDED,
       },
       data: {
         startDate: null,
         endDate: null,
+        status: Status.ACTIVE,
       },
     });
 
-    console.log(`Lifted up users: ${findMany.count}`);
+    console.log(`user set as active from suspended: ${findMany.count}`);
   }
 }
