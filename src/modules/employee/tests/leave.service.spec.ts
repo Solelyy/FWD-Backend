735a6b74fb9338cb52c4a2035dbd014ff46f390b
dinavelB <binongo.dinavel.escodero@gmail.com>
2026-04-22 16:26:12 +0800

    adding leave creation employee side

---
+++ src/modules/employee/tests/leave.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { LeaveService } from '../service/leave.service';
+
+describe('LeaveService', () => {
+  let service: LeaveService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LeaveService],
+    }).compile();
+
+    service = module.get<LeaveService>(LeaveService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
---
+++ src/modules/employee/types/create-leave.ts
@@ -0,0 +1,6 @@
+export enum LeaveEnum {
+  SICK,
+  VACATION,
+  ACCUMULATED,
+  OTHER,
+}
