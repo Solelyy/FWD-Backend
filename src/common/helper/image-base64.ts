a7c0d915e7053fbb9c15f3ae3ff8de2c01584071
dinavelB <binongo.dinavel.escodero@gmail.com>
2026-04-26 11:38:40 +0800

    return original base url of image

--- src/common/helper/image-base64.ts
+++ src/common/helper/image-base64.ts
@@ -6,9 +6,12 @@ export class ImageConfigs {
   async saveBase64Image(base64Data: string) {
     const matches = base64Data.match(/^data:image\/(\w+);base64,(.+)$/);
     const convertStringToByte = Buffer.from(matches![2], 'base64');
+    const imageType = matches![1];
 
     const resizeImg = await sharp(convertStringToByte).resize(300).toBuffer();
-    const resizedBase64 = resizeImg.toString('base64');
+
+    const reziedImage = resizeImg.toString('base64');
+    const resizedBase64 = `data:image/${imageType};base64,${reziedImage}`;
 
     return resizedBase64;
   }
