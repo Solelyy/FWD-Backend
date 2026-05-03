7b165120c4e70b47edb947706d9fcc8427d90cbd
dinavelB <binongo.dinavel.escodero@gmail.com>
2026-05-03 14:59:31 +0800

    .

--- src/modules/employee/controlller/dashboard.controller.ts
+++ src/modules/employee/controlller/dashboard.controller.ts
@@ -86,7 +86,9 @@ export class DashboardController {
   async getCoworkers() {
     const service = await this.service.getCoworkerAttendance();
 
-    return service;
+    return {
+      data: service,
+    };
   }
 
   @Get('my-requests')
